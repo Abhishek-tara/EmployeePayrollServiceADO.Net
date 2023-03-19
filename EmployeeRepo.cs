fed0a1fe706c6f6ad92647c680d36fe7f661b1dd
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-19 12:46:40 +0530

    Retrieving data from given range

--- EmployeeRepo.cs
+++ EmployeeRepo.cs
@@ -185,6 +185,48 @@ namespace EmployeePayRoll
             return result;
         }
 
+        //Usecase 5: Finds the employees in a given range from start date to current
+        public string DataBasedOnDateRange()
+        {
+            string nameList = "";
+            try
+            {
+                using (this.connection)
+                {
+                    //query execution
+                    string query = @"select * from employee_payroll where StartDate BETWEEN Cast('2019-12-13' as Date) and GetDate();";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    //open sql connection
+                    connection.Open();
+
+                    SqlDataReader sqlDataReader = command.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            GetAllEmployee();
+                            nameList += sqlDataReader["EmployeeName"].ToString() + " ";
+                        }
+                    }
+                    //close reader
+                    sqlDataReader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+            }
+            //returns the count of employee in the list between the given range
+            return nameList;
+
+        }
+
     }
 }
 
--- SQLStoreProcedure.sql
+++ SQLStoreProcedure.sql
@@ -21,3 +21,31 @@ insert into employee values(@EmployeeName,@PhoneNumber,@Address,@Department,@Gen
 ,@TaxablePay,@Tax,@NetPay,@StartDate,@City,@Country)
 
 end
+go
+
+create   procedure spUpdateSalary
+(
+@EmployeeName varchar(255),
+@Salary int,
+@PhoneNumber varchar(255),
+@Address varchar(255),
+@Department varchar(255),
+@Gender char(1),
+@City varchar(100),
+@Country varchar(100),
+@BasicPay float,
+@Deductions float,
+@TaxablePay float,
+@Tax float,
+@NetPay float,
+@StartDate Date
+
+)
+as
+begin
+update SALARY
+set EMPSAL=@salary
+where name=@EmployeeName;
+end
+go
+
